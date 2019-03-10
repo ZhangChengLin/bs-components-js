7faac6070663a14018666399200eb915b2297711
lin <469946668@qq.com>
2019-03-11 00:05:11 +0800

    Initial commit

---
+++ dist/bootstrap-modal.js
@@ -0,0 +1,15 @@
+/*!
+ * Name: bootstrap-modal
+ * Version: 0.0.1-alpha
+ * Author: 张成林
+ * Email: 469946668@qq.com
+ * Description: Bootstrap modal
+ * Copyright (c) 2019 张成林
+ * Licenses: MIT
+ * under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ * GitHub: https://github.com/zhangchenglin/bootstrap-modal
+ */
+
+function bootstrapModal() {
+
+}
---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require("gulp");
+const terser = require("gulp-terser");
+const header = require("gulp-header");
+const rename = require("gulp-rename");
+
+//path
+const jquery_path = './node_modules/jquery/dist/jquery.min.js';
+const bootstrap_css_path = './node_modules/bootstrap/dist/css/bootstrap.min.css';
+const bootstrap_js_path = './node_modules/bootstrap/dist/js/bootstrap.min.js';
+const popper_js_path = './node_modules/popper.js/dist/umd/popper.min.js';
+const modal_path = "./src/bootstrap-modal.js";
+
+//header Copyright
+const pkg = require('./package.json');
+const Copyright = {
+    full:
+        '/*!\n' +
+        ' * Name: <%= package.name %>\n' +
+        ' * Version: <%= package.version %>\n' +
+        ' * Author: <%= package.author %>\n' +
+        ' * Email: <%= package.authorEmail %>\n' +
+        ' * Description: <%= package.description %>\n' +
+        ' * Copyright (c) ' + new Date().getFullYear() + ' <%= package.author %>\n' +
+        ' * Licenses: <%= package.license %>\n' +
+        ' * <%= package.licenseDescription %>\n' +
+        ' * GitHub: <%= package.GitHub %>\n' +
+        ' */\n\n',
+    min: ""
+};
+
+gulp.task("min", min);
+gulp.task("copy_static", copy_static);
+
+function min(done) {
+    gulp.src([modal_path])
+        .pipe(terser({}))
+        .pipe(header(Copyright.full, {package: pkg}))
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(gulp.dest("./dist/"));
+    gulp.src([modal_path])
+        .pipe(header(Copyright.full, {package: pkg}))
+        .pipe(gulp.dest("./dist/"));
+    done();
+}
+
+function copy_static(done) {
+    gulp.src([jquery_path, bootstrap_js_path, bootstrap_css_path, popper_js_path]).pipe(gulp.dest('./demoFiles/'));
+    done();
+}
