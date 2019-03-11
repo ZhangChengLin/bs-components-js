9a6d908b3de2454d90402652597acfe615c555c6
lin <469946668@qq.com>
2019-03-12 01:23:03 +0800

    Initial commit

--- dist/bootstrap-modal-js.js
+++ dist/bootstrap-modal-js.js
@@ -1,6 +1,6 @@
 /*!
  * Name: bootstrap-modal-js
- * Version: 0.0.1-alpha.1
+ * Version: 0.0.1-alpha.2
  * Author: 张成林
  * Email: 469946668@qq.com
  * Description: Bootstrap modal
@@ -43,6 +43,8 @@ function bootstrapModalJs(title, body, footer, ModalSizes, VerticallyCentered, L
 
     modal_dialog.className = "modal-dialog";
     switch (ModalSizes) {
+        case "default":
+            break;
         case "sm":
             modal_dialog.className += " modal-sm";
             break;
@@ -51,8 +53,6 @@ function bootstrapModalJs(title, body, footer, ModalSizes, VerticallyCentered, L
             break;
         case "xl":
             modal_dialog.className += " modal-xl";
-            break;
-        case "default":
             break;
         default:
             modal_dialog.className += " " + ModalSizes;
@@ -75,7 +75,7 @@ function bootstrapModalJs(title, body, footer, ModalSizes, VerticallyCentered, L
         case false:
             break;
         default:
-            modal_dialog.className += " " + VerticallyCentered;
+            modal_dialog.className += " " + LongContentType;
     }
     modal_dialog.role = "document";
 
@@ -111,7 +111,7 @@ function bootstrapModalJs(title, body, footer, ModalSizes, VerticallyCentered, L
     modal.appendChild(modal_dialog);
     document_body.appendChild(modal);
 
-    EventType && CallbackFunction ? bootstrap_modal_events(modal_ID) : "";
+    EventType && CallbackFunction ? bootstrap_modal_events(modal_ID, EventType, CallbackFunction) : "";
 
     $("#" + modal_ID).modal("show");
     removeBootstrapModal(modal_ID);
@@ -126,6 +126,31 @@ function removeBootstrapModal(modal_id) {
     });
 }
 
-function bootstrap_modal_events(modal_id) {
+function bootstrap_modal_events(modal_id, type, fun) {
+    const modal = $("#" + modal_id);
+    switch (type) {
+        case "show":
+            modal.on("show.bs.modal", function () {
+                return fun();
+            });
+            break;
+        case "shown":
+            modal.on("shown.bs.modal", function () {
+                return fun();
+            });
+            break;
+        case "hide":
+            modal.on("hide.bs.modal", function () {
+                return fun();
+            });
+            break;
+        case "hidden":
+            modal.on("hidden.bs.modal", function () {
+                return fun();
+            });
+            break;
+        default:
+
+    }
 
 }
