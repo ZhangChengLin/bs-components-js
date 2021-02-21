3ca64ecd13ad7e6161e8c327b42a3b2edc6223ad
zhangchenglin <469946668@qq.com>
2021-02-21 21:30:52 +0800

    bootstrap@5.0.0-beta2

--- dist/bootstrap-modal-js.js
+++ dist/bootstrap-modal-js.js
@@ -1,10 +1,10 @@
 /*!
  * Name: bootstrap-modal-js
- * Version: 2.0.0-beta1
+ * Version: 2.0.0-beta2
  * Author: 张成林
  * Email: 469946668@qq.com
  * Description: Bootstrap modal
- * Copyright (c) 2020 张成林
+ * Copyright (c) 2021 张成林
  * Licenses: MIT
  * under the MIT License (license terms are at https://opensource.org/licenses/MIT).
  * GitHub: https://github.com/zhangchenglin/bootstrap-modal-js
