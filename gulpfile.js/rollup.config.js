56542a45318d24aebdb4ead8f9960f0c15ed09c9
zhangchenglin <469946668@qq.com>
2021-12-13 06:43:42 +0800

    2.0.2 init build

--- gulpfile.js/rollup.config.js
+++ gulpfile.js/rollup.config.js
@@ -1,19 +1,32 @@
 'use strict'
 
+const {terser} = require('rollup-plugin-terser')
+
 const banner = require('./banner.js')
 const {paths} = require('./gulpfile.paths')
 
 const rollupConfig = {
   input: [paths.src + 'offcanvas/index.umd.js'],
-  output: {
-    banner,
-    file: paths.dist + 'bootstrapOffcanvasJs.js',
-    format: 'umd',
-    generatedCode: 'es2015',
-    sourcemap: true
-  },
+  output: [
+    {
+      banner,
+      file: paths.dist + 'bootstrapOffcanvasJs.js',
+      format: 'umd',
+      generatedCode: 'es2015',
+      sourcemap: true
+    },
+    {
+      banner,
+      file: paths.dist + 'bootstrapOffcanvasJs.min.js',
+      format: 'umd',
+      generatedCode: 'es2015',
+      plugins: [terser()],
+      sourcemap: true
+    },
+  ]
 }
 
-rollupConfig.output.name = 'bootstrapOffcanvasJs'
+rollupConfig.output[0].name = 'bootstrapOffcanvasJs'
+rollupConfig.output[1].name = 'bootstrapOffcanvasJs'
 
 module.exports = rollupConfig
